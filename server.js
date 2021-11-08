e02ce56c0dadfac820fce0a2e2acb40ea51294cb
AngiePilo99 <90914883+AngiePilo99@users.noreply.github.com>
2021-11-07 20:55:50 -0500

    GET fUNCIONANDO

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const  mongoose  = require('mongoose');
 
 const app = express();
-const route = express.Router();
+const router = express.Router();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
@@ -10,7 +10,13 @@ app.use(express.json());
 mongoose.connect("mongodb+srv://AngiePilo:piloenbici22@clusterc4g41.qrab0.mongodb.net/ejercicio1realDB?retryWrites=true&w=majority");
 
 //OPERACIONES CRUD
-app.use(route);
+
+router.get('/',(req, res) => {
+    res.send("Este es el inicio del programa VAMOS POR PAPITAS STEVE")
+})
+
+
+app.use(router);
 app.listen(3000, ()=> {
     console.log("servidor corriendo en el puerto 3000")
 })
